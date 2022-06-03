073f973eec0827ccb297e0bdf78c19603e264280
zionesaidi <zilansaidi@gmail.com>
2022-06-03 13:00:23 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
@@ -7,3 +19,5 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
+}
