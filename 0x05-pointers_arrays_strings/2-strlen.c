88b6a3f26ca4135fedad0dd80693f4f8dd0ade3a
zionesaidi <zilansaidi@gmail.com>
2022-06-13 03:52:31 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ *   @s: give me the word
+ *   Return: gave large
+ */
+int _strlen(char *s)
+{
+	int large = 0;
+
+			while (*(s + large) != '\0')
+					{
+						large++;
+					}
+				return (large);
+}
