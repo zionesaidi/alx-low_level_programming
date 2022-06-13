bf910c43c2b49b709c861ddbb9b0c2b4005e053a
zionesaidi <zilansaidi@gmail.com>
2022-06-13 04:03:12 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - Entry point.
+ *  * @str: the variable
+ */
+void _puts(char *str)
+{
+		int msg;
+
+			for (msg = 0; *(str + msg) != '\0'; msg++)
+			{
+			   	_putchar(str[msg]);
+			}
+                        putchar('\n');
+}
