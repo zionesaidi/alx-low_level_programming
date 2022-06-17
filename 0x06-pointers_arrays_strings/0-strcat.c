bdaf1cc9a221369d0d60b01ae338ccf66384f220
zionesaidi <zilansaidi@gmail.com>
2022-06-16 18:36:24 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-		int dlen = 0, i;
+	int c, c2;
+	c = 0;
 
-			while (dest[dlen])
-			{
-				dlen++;
-			}
-
-			for (i = 0; src[i] != 0; i++)
-			{
-				dest[dlen] = src[i];
-				dlen++;
-			}
-			dest[dlen] = '\0';
-			return (dest);
+	/*find the size of dest array*/
+	while (dest[c])
+	c++;
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+	/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+	dest[c++] = src[c2];
+	return (dest);
 }
